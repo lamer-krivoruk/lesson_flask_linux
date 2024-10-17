610f4622e984ef0f87e64d4e361bd28e204038c4
Konstantin Telminov <kostik.telminov.80@mail.ru>
2024-10-17 15:05:53 +0300

    write wsgi

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+# 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
+from doctest import debug
+
+from lesson_1.app import app
+# from lesson_2.app import app
+# from lesson_3.app import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
